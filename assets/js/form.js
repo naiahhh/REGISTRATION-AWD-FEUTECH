14c088047f87ff69c05b80ddfd2f341b6228f64c
Dalet <104555136+Spod101@users.noreply.github.com>
2024-03-10 08:59:45 +0800

    Added a validation feature for inputs

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,15 +6,37 @@ $(document).ready(function(){
       $(this).addClass("active");
       $(".register_li").removeClass("active");
       $(".login").show();
-       $(".register").hide();
+      $(".register").hide();
     })
 
     $(".register_li").click(function(){
       $(this).addClass("active");
       $(".login_li").removeClass("active");
       $(".register").show();
-       $(".login").hide();
-       $(".form-floating mb-3").hide();
+      $(".login").hide();
+      $(".form-floating mb-3").hide();
     })
 });
 
+$(document).ready(function() {
+  function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  $('.btn a').click(function(event) {
+    event.preventDefault();
+
+    var $form = $(this).closest('div.right').find('.register:visible, .login:visible');
+    var $emailInput = $form.find('input[type="email"]');
+    var email = $emailInput.val();
+
+    if (!validateEmail(email)) {
+      alert('Please enter a valid email address.');
+      $emailInput.focus();
+      return;
+    }
+
+    alert('Form submitted successfully.');
+  });
+});
